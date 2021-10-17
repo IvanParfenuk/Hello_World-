8da8d69ed53fb8a2c44e6d78e2d409821bac63a8
name <name@bk.ru>
2021-10-17 14:02:24 +0300

    teleBot

--- Telegram/main.py
+++ Telegram/main.py
@@ -1,17 +1,26 @@
+import telebot
 
-from aiogram import Bot, Dispatcher, types
+bot =telebot.TeleBot('2098527930:AAGW6h800_oqeNCYbxX-axLsehlqzEtt-VA')
 
-bot = Bot('2098527930:AAGW6h800_oqeNCYbxX-axLsehlqzEtt-VA')
-dp = Dispatcher(bot)
+@bot.message_handler(commands=["start"])
+def start(message):
+    send_message = 'Привет'
+    bot.send_message(message.chat.id, send_message)
 
-@dp.message_handler(commands="start")
-async def cmd_start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup()
-    button_1 = types.KeyboardButton(text="С пюрешкой")
-    keyboard.add(button_1)
-    button_2 = "Без пюрешки"
-    keyboard.add(button_2)
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+bot.polling(none_stop=True)
+
+
+
+
+
+# async def cmd_start(message: types.Message):
+#     keyboard = types.ReplyKeyboardMarkup()
+#     button_1 = types.KeyboardButton(text="С пюрешкой")
+#     keyboard.add(button_1)
+#     button_2 = "Без пюрешки"
+#     keyboard.add(button_2)
+#     await message.answer("Как подавать котлеты?", reply_markup=keyboard)
 
 
 
