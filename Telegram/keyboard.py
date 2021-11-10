ed1018ea7375687d82e71e3d516af075b4b9ce2e
name <name@bk.ru>
2021-11-10 17:07:33 +0300

    teleBot

--- Telegram/keyboard.py
+++ Telegram/keyboard.py
@@ -9,17 +9,22 @@ markup.row(eat_but, eat_1but) #creating the first row of buttons
 markup.row(eat_2but, o_nas) #creating a second row of buttons
 
 
+markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)  # creating a keyboard
+_but = types.KeyboardButton('◀')
+# _1but = types.KeyboardButton('⬇')
+_2but = types.KeyboardButton('▶')
+back = types.KeyboardButton('Назад в меню')
+markup2.row(_but, _2but) #creating the first row of buttons
+markup2.row(back)
+
+
+
+
+
 # # keyboard for rewinding
-eda_ = types.InlineKeyboardMarkup()
-but_1 = types.InlineKeyboardButton(text="◀", callback_data="Вперед")
-but_2 = types.InlineKeyboardButton(text="⬇", callback_data="Добавить в корзину")
-but_3 = types.InlineKeyboardButton(text="▶", callback_data="Назад")
-eda_.add(but_1, but_2, but_3) # adding buttons to the keyboard eda_
+# eda_ = types.InlineKeyboardMarkup()
+# but_1 = types.InlineKeyboardButton(text="◀", callback_data="Вперед")
+# but_2 = types.InlineKeyboardButton(text="⬇", callback_data="Добавить в корзину")
+# but_3 = types.InlineKeyboardButton(text="▶", callback_data="Назад")
+# eda_.add(but_1, but_2, but_3) #adding buttons to the keyboard eda_
 
-
-# pizza1 = types.InlineKeyboardMarkup()
-# but_1 = types.InlineKeyboardButton(
-#     text="Оформить заказ", url="https://t.me/por0vos1k")
-# but_2 = types.InlineKeyboardButton(text="▶️", callback_data="Вперед1")
-# pizza1.add(but_2)
-# pizza1.add(but_1)
--- Telegram/main.py
+++ Telegram/main.py
@@ -1,35 +1,52 @@
 import telebot
 from Telegram import keyboard
 
-bot =telebot.TeleBot('2098527930:AAGW6h800_oqeNCYbxX-axLsehlqzEtt-VA')
+bot = telebot.TeleBot('2098527930:AAGW6h800_oqeNCYbxX-axLsehlqzEtt-VA')
 
+# Пропишем описание для каждой пиццы
+text_about_dobrynya = "Добрыня:\nпицца-соус, сыр Моцарелла, грудинка, креветки, шампиньоны, томаты, перец сладкий, специи 465 г"
+text_about_man = "Мужская(острая):\nпицца-соус, сыр Моцарелла, колбаса сырокопченая, ветчина, филе птицы, свинина отварная, маслины, масло оливковое, кайенский перец, специи 430 г"
+text_about_mushrooms = 'Грибная:\nпицца-соус, сыр Моцарелла, шампиньоны, соевый соус, лук-порей, специи 455 г'
+
+# Создадим словарь
+dict_pizzas = {'Добрыня': (open('Pizza-Dobrynya.jpg', 'rb'), text_about_dobrynya),
+               'Мужская': (open('Pizza-Muzhskaya-ostraya-300x300.jpg', 'rb'), text_about_man),
+               'Грибная': (open('Pizza-Gribnaya.jpg', 'rb'), text_about_mushrooms)}
+
+# Приветствие на команду старт
 @bot.message_handler(commands=['start'])
 def begin(message):
     sti = 'Привет, что желаете заказать?'
-    bot.send_message(message.chat.id, sti, reply_markup=keyboard.markup)  # welcome message
+    bot.send_message(message.chat.id, sti, reply_markup=keyboard.markup) # welcome message
 
+# Ответ на команды типа: текст
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text == '💌 О нас':
         doc = open('fortg.txt', 'rb')
-        bot.send_document(message.chat.id, doc)
+        bot.send_document(message.chat.id, doc, reply_markup=keyboard.markup)
+    if message.text == 'Пицца🍕':
+        bot.send_photo(message.chat.id, photo=dict_pizzas['Добрыня'][0], caption=dict_pizzas['Добрыня'][1], reply_markup=keyboard.markup2) # Обращаюсь в словарь к ключу "Добрыня", затем через индекс обращаюсь к нужному значению
+    if message.text == '▶':
+        bot.send_photo(message.chat.id, photo=dict_pizzas['Мужская'][0], caption=dict_pizzas['Мужская'][1], reply_markup=keyboard.markup2) # Обращаюсь в словарь к ключу "Мужская", затем через индекс обращаюсь к нужному значению
+    elif message.text == '◀':
+        bot.send_photo(message.chat.id, photo=dict_pizzas['Грибная'][0], caption=dict_pizzas['Грибная'][1], reply_markup=keyboard.markup2) # Обращаюсь в словарь к ключу "Грибная", затем через индекс обращаюсь к нужному значению
+    elif message.text == 'Назад в меню':
+        transition_text = 'Пожалуйста, вы в главном меню😋'
+        bot.send_message(message.chat.id, transition_text, reply_markup=keyboard.markup)
 
-    elif message.text == 'Пицца🍕':
-        description = 'Добрыня:\n(тонкое тесто)пицца-соус, сыр Моцарелла, грудинка, креветки, шампиньоны, томаты, перец сладкий, специи 465 г'
-        bot.send_photo(message.chat.id, photo=open('Pizza-Dobrynya.jpg', 'rb'), caption=description, reply_markup=keyboard.eda_)
 
-    elif message.text == 'Горячие закуски🧆':
-        description = 'Чикен Бастони:\nфиле птицы в панировке, соус Чили 150/30 г'
-        bot.send_photo(message.chat.id, photo=open('Chiken-Bastonni.jpg', 'rb'), caption=description, reply_markup=keyboard.eda_)
 
-    else:
-        description = 'Цезарь с креветками:\nкоролевские креветки, сыр Джюгас, томаты, салат Айсберг, чесночные гренки, зелень, соус Цезарь 230 г'
-        bot.send_photo(message.chat.id, photo=open('Salat-Tsezar-s-krevetkami.jpg', 'rb'), caption=description, reply_markup=keyboard.eda_)
 
+#     elif message.text == 'Горячие закуски🧆':
+#         description = 'Чикен Бастони:\nфиле птицы в панировке, соус Чили 150/30 г'
+#         bot.send_photo(message.chat.id, photo=open('Chiken-Bastonni.jpg', 'rb'), caption=description, reply_markup=keyboard.markup2)
+#
+#     elif message.text == 'Салаты🥗':
+#         description = 'Цезарь с креветками:\nкоролевские креветки, сыр Джюгас, томаты, салат Айсберг, чесночные гренки, зелень, соус Цезарь 230 г'
+#         bot.send_photo(message.chat.id, photo=open('Salat-Tsezar-s-krevetkami.jpg', 'rb'), caption=description, reply_markup=keyboard.markup2)
+#
 bot.polling(none_stop=True)
-
-
-
 
 
 
